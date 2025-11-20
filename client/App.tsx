f5d1e44a83c9e7c79ecbb9ea97f74c9c5100ab7d
Yuvraj <yuvrajpadhy@gmail.com>
2025-11-20 13:15:28 +0530

    File moved to features

--- client/App.tsx
+++ client/App.tsx
@@ -7,19 +7,19 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import NotFound from "./feature/notfound/pages/NotFound";
 import { VerificationProgressPage } from "./components/VerificationProgressPage";
 import { VerificationSuccessPage } from "./components/VerificationSuccessPage";
 import { AuthLoginPage } from "./components/AuthLoginPage";
 import { AuthOtpPage } from "./components/AuthOtpPage";
 import { Dashboard } from "./components/Dashboard";
-import Profile from "./pages/Profile";
+import Profile from "./feature/profile/pages/Profile";
 import { PasswordSetupPage } from "./components/PasswordSetupPage";
 import { OngoingVerificationPage } from "./components/OngoingVerificationPage";
 import { ExpiredVerificationPage } from "./components/ExpiredVerificationPage";
 import { VerifiedCredentialsPage } from "./components/VerifiedCredentialsPage";
 import { ContactAdminPage } from "./components/ContactAdminPage";
-import HandoffPage from "./pages/HandoffPage";
+import HandoffPage from "@/feature/Handoff/pages/HandoffPage";
 import { PasswordSetupPage as PWSetUp } from "./components/PasswordSetupPage";
 
 //1st change
